b87ec71850b267d0d9c9d816411d0952f6c0a82f
196 <kankou.chin@gmail.com>
2022-10-23 14:43:27 +0900

    feature: main components

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
+      <!-- <v-card class="logo py-4 d-flex justify-center">
         <NuxtLogo />
         <VuetifyLogo />
-      </v-card>
-      <v-card>
-        {{ hello }}
-      </v-card>
+      </v-card> -->
+      <v-container>
+        <ShowNumberArea />
+        <ButtonsArea />
+      </v-container>
     </v-col>
   </v-row>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue'
-const hello = ref('hello')
-</script>
+<script setup lang="ts"></script>
