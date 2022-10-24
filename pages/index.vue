1fed26567e1a7fc08dcef24d8beb709311794604
196 <kankou.chin@gmail.com>
2022-10-24 09:57:16 +0900

    feat: base logic

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,15 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <!-- <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card> -->
       <v-container>
-        <ShowNumberArea />
-        <ButtonsArea />
+        <ShowNumberArea :show="a" />
+        <ButtonsArea @onClick="(e) => a = e" />
       </v-container>
     </v-col>
   </v-row>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const a = ref('')
+</script>
