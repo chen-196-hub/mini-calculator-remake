d20cbad990b1212fa348f5d3daa09814565343c6
196 <kankou.chin@gmail.com>
2022-10-29 00:00:00 +0900

    fix: jest for vuetify

--- components/ShowNumberArea.vue
+++ components/ShowNumberArea.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card class="text-center py-3 my-3">
-    {{ number || '0' }}
+    <v-card-title> {{ number }} </v-card-title>
   </v-card>
 </template>
 
 <script setup lang="ts">
 interface Props {
-  number: string
+  number?: string
 }
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const props = withDefaults(defineProps<Props>(), {
