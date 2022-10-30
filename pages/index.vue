0444ea9e0b9893a0deef56ea8427c285e59a103e
196 <kankou.chin@gmail.com>
2022-10-30 23:52:40 +0900

    feat: + _ * / && fix snap

--- pages/index.vue
+++ pages/index.vue
@@ -16,13 +16,32 @@ interface ShowNumber {
   show: string
   calc: string[]
 }
+// interface calcFlag {
+//   ac: boolean,
+//   pluMin: boolean
+//   percent: boolean
+//   divide: boolean
+//   times: boolean
+//   minus: boolean
+//   plus: boolean
+//   equal: boolean
+// }
+
+interface CalcStatus {
+  isCalc: boolean
+  calcType: string
+}
+enum Calc {
+  plus = 'plus',
+  minus = 'minus',
+  times = 'times',
+  divide = 'divide',
+}
 const showNumber = reactive<ShowNumber>({
   show: '0',
   calc: ['0'],
 })
-const firstNumber = ref(0)
-// const secondNumber = ref(0)
-// const processKeys = ref()
+const firstNumber = ref<number>(0)
 const numbersKeys = ref([
   '0',
   '1',
@@ -36,6 +55,19 @@ const numbersKeys = ref([
   '9',
   'ten',
 ])
+
+// const calcFlag = reactive<calcFlag>({
+//   ac: false,
+//   pluMin: false,
+//   percent: false,
+//   divide: false,
+//   times: false,
+//   minus: false,
+//   plus: false,
+//   equal: false,
+// })
+// const isCalc = ref(false)
+const calcStatus = reactive<CalcStatus>({ isCalc: false, calcType: '' })
 
 const calcs = ref<string[]>([
   'ac',
@@ -54,14 +86,15 @@ const handleClick = (e: string = '0') => {
   }
   if (numbersKeys.value.includes(e)) {
     handleNumber(e)
+    showNumber.show = showNumber.calc.join('')
   }
-  showNumber.show = showNumber.calc.join('')
 }
 
 const handleCalc = (key: string): void => {
   switch (key) {
     case 'ac':
       showNumber.calc = ['0']
+      showNumber.show = showNumber.calc.join('')
       break
     case 'plu-min':
       showNumber.calc[0] !== '-'
@@ -71,27 +104,52 @@ const handleCalc = (key: string): void => {
     case 'percent':
       break
     case 'divide':
-      break
-    case 'times':
-      break
-    case 'minus':
-      break
-    case 'plus':
+      calcStatus.calcType = Calc.divide
       if (firstNumber.value === 0) {
         firstNumber.value = Number(showNumber.show)
-      } else {
-        // firstNumber.value + Number(showNumber.show)
       }
-      // console.log(firstNumber.value)
+      calcStatus.isCalc = true
       break
-    case 'equal':
+    case 'times':
+      calcStatus.calcType = Calc.times
+      if (firstNumber.value === 0) {
+        firstNumber.value = Number(showNumber.show)
+      }
+      calcStatus.isCalc = true
       break
+    case 'minus':
+      calcStatus.calcType = Calc.minus
+      if (firstNumber.value === 0) {
+        firstNumber.value = Number(showNumber.show)
+      }
+      calcStatus.isCalc = true
+      break
+    case 'plus':
+      calcStatus.calcType = Calc.plus
+      if (firstNumber.value === 0) {
+        firstNumber.value = Number(showNumber.show)
+      }
+      calcStatus.isCalc = true
+      break
+    case 'equal': {
+      const answer = calc(
+        firstNumber.value,
+        Number(showNumber.show),
+        calcStatus.calcType
+      )
+      showNumber.show = String(answer)
+      break
+    }
     default:
       break
   }
 }
 
 const handleNumber = (key: string): void => {
+  if (calcStatus.isCalc) {
+    showNumber.calc = ['0']
+    calcStatus.isCalc = false
+  }
   const num: string = key === 'ten' ? '.' : key
   if (
     showNumber.calc[0] === '0' &&
@@ -101,5 +159,24 @@ const handleNumber = (key: string): void => {
     showNumber.calc.shift()
   }
   showNumber.calc.push(num)
+}
+
+const calc = (
+  firstNumber: number,
+  secondNumber: number,
+  calcType: string
+): number => {
+  switch (calcType) {
+    case 'plus':
+      return firstNumber + secondNumber
+    case 'minus':
+      return firstNumber - secondNumber
+    case 'times':
+      return firstNumber * secondNumber
+    case 'divide':
+      return firstNumber / secondNumber
+    default:
+      return 0
+  }
 }
 </script>
